52f21f3a85f0c7de641d40cc048c78af36733f96
Mintu <pmk80028@gmail.com>
2025-09-03 21:28:17 +0530

    understand about data types in JawaScript

---
+++ dataTypes.js
@@ -0,0 +1,42 @@
+let length = 5.7
+// console.log(typeof length);
+
+let name = "mintu"
+// console.log(typeof name);
+
+let age = 19
+// console.log(typeof age);
+
+let isGraduate = false
+// console.log(typeof isGraduate);
+
+let city 
+// console.log(typeof city);
+
+let a = 16.5 + 5
+// console.log(a ,typeof a);
+let b = 5 + 'b'
+// console.log(b , typeof b);
+
+let c = 5 + 7 + 'abcd'
+// console.log(c , typeof c);
+
+
+let d = 'abcd' + 5 + 7
+// console.log(d , typeof d);
+
+
+
+let e = 'a' + 'b' + 'c'
+// console.log(e , typeof e);
+
+let f = 5 + 5 + 'ab'
+// console.log(f , typeof f);
+
+let g = 'ab' + 5+5
+console.log(g , typeof g);
+
+
+
+
+
