54b9183f7d8237a0046df2e2f4d2972996e4413d
Mintu <pmk80028@gmail.com>
2025-09-04 12:01:15 +0530

    practice data Types

--- dataTypes.js
+++ dataTypes.js
@@ -1,42 +1,59 @@
-let length = 5.7
-// console.log(typeof length);
+// let length = 5.7
+// // console.log(typeof length);
 
-let name = "mintu"
-// console.log(typeof name);
+// let name = "mintu"
+// // console.log(typeof name);
 
-let age = 19
-// console.log(typeof age);
+// let age = 19
+// // console.log(typeof age);
 
-let isGraduate = false
-// console.log(typeof isGraduate);
+// let isGraduate = false
+// // console.log(typeof isGraduate);
 
-let city 
-// console.log(typeof city);
+// let city 
+// // console.log(typeof city);
 
-let a = 16.5 + 5
-// console.log(a ,typeof a);
-let b = 5 + 'b'
-// console.log(b , typeof b);
+// let a = 16.5 + 5
+// // console.log(a ,typeof a);
+// let b = 5 + 'b'
+// // console.log(b , typeof b);
 
-let c = 5 + 7 + 'abcd'
-// console.log(c , typeof c);
-
-
-let d = 'abcd' + 5 + 7
-// console.log(d , typeof d);
+// let c = 5 + 7 + 'abcd'
+// // console.log(c , typeof c);
 
 
-
-let e = 'a' + 'b' + 'c'
-// console.log(e , typeof e);
-
-let f = 5 + 5 + 'ab'
-// console.log(f , typeof f);
-
-let g = 'ab' + 5+5
-console.log(g , typeof g);
+// let d = 'abcd' + 5 + 7
+// // console.log(d , typeof d);
 
 
 
+// let e = 'a' + 'b' + 'c'
+// // console.log(e , typeof e);
+
+// let f = 5 + 5 + 'ab'
+// // console.log(f , typeof f);
+
+// let g = 'ab' + 5+5
+// console.log(g , typeof g);
+
+let a = "10";
+let b = 10;
+
+// console.log(typeof a , typeof b);
+
+let velue = true;
+// console.log(typeof velue);
+
+let x;
+// console.log(x);
+// console.log(typeof x);
 
 
+let arr = [1,2,3];
+// console.log(typeof arr);
+
+// console.log(typeof null);
+
+// console.log(typeof NaN);
+
+// console.log(typeof typeof 123);
