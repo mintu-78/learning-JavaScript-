ed9064779a6ca5ec75db98376332d3d41dfd8513
Mintu <pmk80028@gmail.com>
2025-09-04 11:55:55 +0530

    Learn about variable declarition in JavaScript(ver,let,const)

---
+++ variableDeclaration.js
@@ -0,0 +1,56 @@
+
+// // var
+
+// var name = "Raj"; // first time declaration
+// console.log(name);
+
+// var name = "Mintu"; // re- declaration
+// console.log(name);
+
+// name = "deepak" // update name 
+// console.log(name);
+
+var data;
+
+data = 20;
+console.log(data);
+
+
+// let
+
+// let age = 20;
+// console.log(20);
+
+// // let age = 30; // can't re-declare
+
+// age = 30;
+// console.log(age)
+
+// {
+//     let age = 40;
+//     console.log(age)
+// }
+
+// console.log(age)
+
+
+// let abc;
+
+// abc = 40;
+// console.log(abc);
+
+//Const//
+const city = "suriya";
+console.log(city);
+
+// const name; 
+// name = "Mukesh" // cannot assing value later
+
+// city = "Hazaribag" // cannot update
+console.log(city);
+
+
+
+
+
+
