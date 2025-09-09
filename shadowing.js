a60f3ce1118b97c4e5abff1bef67afef26fe3cd6
Mintu <pmk80028@gmail.com>
2025-09-09 18:59:01 +0530

    understand about shadowing in JavaScript

---
+++ shadowing.js
@@ -0,0 +1,65 @@
+// // example 1 
+
+// let a = 50;
+// console.log('a=',a); //50
+
+// {
+//     let a = 10;
+//     console.log('a =',a); //10
+// }
+
+// console.log('a =' ,a); //50
+
+
+// // example 2 
+
+// var b = 10;
+// console.log('b =' , b); //10
+// {
+//     var b = 5;
+//     console.log('b = ',b); //5
+// }
+
+// console.log('b = ',b);  //5
+// var b = 100;
+// console.log('b = ',b); //100
+
+// console.log('b = ',b);  //100
+
+
+// {
+//     var b = 10;
+//     console.log('b = ',b); //10
+// }
+
+// console.log('b = ',b); //10
+
+// var b = 80;
+// console.log('b = ',b); //80
+
+
+// // ecample 3
+
+// // let a = 10;
+
+// // {
+// //     var a =  10;
+// //     console.log(a);
+    
+// // }
+
+
+// // example 4
+
+// var c = 10; 
+// {
+//     let c = 10; 
+//     console.log('c =', c); //10
+    
+// }
+
+
+
+
+
+
