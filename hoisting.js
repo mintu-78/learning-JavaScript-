e9bee14984770252e3af672e1b12a788db69a539
Mintu <pmk80028@gmail.com>
2025-09-10 23:54:43 +0530

    learn about hoisting in JavaScript

---
+++ hoisting.js
@@ -0,0 +1,34 @@
+// // variable hoasting.
+// console.log(a);
+// var a;
+// a = 7;
+// console.log(a);
+
+// console.log(b);
+// let b =7;
+
+
+// // function hoisting
+
+
+// fun1()
+
+// function fun1(){
+//     console.log("hello from");
+    
+// }
+
+// fun1()
+
+
+
+// let and const hoisting 
+
+
+// console.log(e);  //here e is hoisted and produce undefined
+// console.log(d); // here d produce and reference error(i.e. d is defined)
+// var e = 10;
+// let d = 20;  //here d is in temporal dead zone
+
+
+
