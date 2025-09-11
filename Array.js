d4745ba81fa911ccbbec8c633d7ed125a7de2316
Mintu <pmk80028@gmail.com>
2025-09-11 21:15:10 +0530

    Learn about Array and Array Methods in JavaScript

---
+++ Array.js
@@ -0,0 +1,125 @@
+let marks = [85,78,84,91,82]
+let name = ['Rohan','Sohan','Mohan','Ram','shyam']
+let email = ['rohan@gmail.com','sohan@gmail.com','mohan@gmail.com','ram@gmail.com','shyam@gmail.com']
+
+
+////push()////
+
+// let arr = [5,7,9,12,15]
+// arr.push(10);
+
+// console.log(arr);//[5,7,9]
+
+// arr.push(15);
+// arr.push(20);
+
+// console.log(arr);//[5,7,9,15,20]
+
+
+
+//**************pop()**************//
+
+// let arr = [6,7,4,9]
+// arr.pop()
+
+// console.log(arr);//[6,7,4]
+
+
+
+
+//**************unshift()**************//
+
+// let arr = [4,6,5]
+// arr.unshift('mintu',[6,6])
+
+// console.log(arr);
+
+
+
+//**************shift()**************//
+
+// let arr = [5,4,7,8,'mintu']
+// arr.shift()
+
+// console.log(arr);
+
+
+
+//**************indexOf()**************//
+
+// let arr = [9,15,17,25]
+// console.log(arr.indexOf(17));
+
+
+//**************includes()**************//
+
+
+// console.log(arr);
+
+// let isinclude = arr.includes(12);
+
+// console.log(isinclude);
+
+// console.log(arr.includes(19));
+
+
+//**************find()**************//
+
+
+// console.log(arr);
+
+// let found = arr.find(el => el > 9)
+
+// console.log(found);
+
+// let dividedBy3 = arr.find(el => el % 3 == 0)
+// console.log(dividedBy3);
+
+
+
+//**************findIndex()***********//
+
+
+// console.log(arr);
+
+// let found = arr.findIndex(el => el > 12)
+// console.log(found);
+
+
+//**************sort()**************//
+
+// let arr = [3,7,4,8,5]
+// // console.log(arr);
+
+// let newArray = arr.sort()
+// console.log(newArray);
+
+
+
+//**************reverse()**************//
+
+let arr = [11,24,31,4,51,6]
+// console.log(arr);
+
+let newArray = arr.reverse()
+
+console.log(arr);
+console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
