498a8a59b71ab1689fbaee8feb0856dff28dbb74
Mintu <pmk80028@gmail.com>
2025-09-12 22:26:11 +0530

    understanding abaut loop in JavaScript

---
+++ loops.js
@@ -0,0 +1,57 @@
+//****************for loop**************//
+
+
+// let arr = [2,11,13,7,6]
+// for(let start = 0; start < arr.length; start++){
+//     console.log(arr[start]);
+    
+// }
+
+
+//****************while loop**************//
+
+// let arr = [4,5,3,6,8,7]
+// let start = 0;
+// while(start < arr.length){
+//     console.log(arr[start])
+//     start++
+    
+// }
+
+
+
+//****************Do-while loop**************//
+
+
+// let arr = [8,3,5,4,2]
+// let start = 0;
+// do{
+//     console.log(arr[start]);
+//     start++
+// }
+// while(start < arr.length)
+
+
+
+//****************for-in loop**************//
+
+// const student = {
+//     name : "Rohan",
+//     age : "28",
+//     city : "mumbai",
+//     state : "maharashtra",
+//     county : "india"
+// }
+// for(let key in student){
+//     console.log(key);
+    
+// }
+
+
+//****************for-of loop**************//
+
+let arr =[11,22,33,55,44,66]
+for(let value of arr){
+    console.log(value);
+    
+}
