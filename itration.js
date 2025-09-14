bb2949a340e13a3c2eff2ec4e2cb747ac3f4d473
Mintu <pmk80028@gmail.com>
2025-09-14 22:35:21 +0530

    learn about  iteration forEach Method in JavaScript

---
+++ itration.js
@@ -0,0 +1,15 @@
+//forEach//
+
+
+
+let arr = [1,3,3,4,6,5]
+
+// arr.forEach( (currentValue , index, array) => {
+//     console.log(currentValue, '-', index, '-', array);
+    
+// })
+
+arr.forEach((currentValue) => {
+    console.log(currentValue);
+    
+})
