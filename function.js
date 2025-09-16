57fb5a95036caa2e22fa93f9f98398727ced6f59
Mintu <pmk80028@gmail.com>
2025-09-16 17:04:34 +0530

    learn about functions in JavaScript

---
+++ function.js
@@ -0,0 +1,62 @@
+// function greet (){
+//     console.log("Hello JavaScript");
+    
+// }
+// greet()
+
+
+
+//***********function expression************//
+
+
+// let greet = function (){
+//     console.log("hello javascript");
+// }
+
+// greet();
+
+
+
+//*********** Anonymous function expression************//
+ 
+
+// const square = function(num){
+//     console.log(num * num);
+    
+// }
+// square(8)
+
+
+
+
+//*********** Arrow function************//
+
+
+
+// const greet = () => {
+//     console.log("hello");
+    
+// }
+// greet()
+
+
+// const square = (num) =>{
+//     console.log(num * num);
+    
+// }
+// square(5)
+
+
+
+
+// const add = (num1, num2) => console.log(num1 + num2);
+
+// add(5,7)
+
+
+
+// const add2 = (num1, num2) =>{
+//     console.log(num1 + num2);
+    
+// }
+// add2(5,5)
