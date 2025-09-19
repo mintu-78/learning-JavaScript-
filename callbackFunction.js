15c8f78edf881ac56db6f1bb2a3fa3d1ba73af4f
Mintu <pmk80028@gmail.com>
2025-09-19 22:25:14 +0530

    practice some questions of callback function

--- callbackFunction.js
+++ callbackFunction.js
@@ -1,12 +1,36 @@
-function callbackFunction(){
-    console.log("Good bye");
+// function callbackFunction(){
+//     console.log("Good bye");
     
+// }
+
+// function myFunction(a){
+//     console.log("history");
+    
+//     a()
+// }
+
+// myFunction(callbackFunction)
+
+
+function greet(name, callback) {
+    console.log("Hi, " + name);
+    callback();
 }
 
-function myFunction(a){
-    console.log("history");
-    
-    a()
+function sayWelcome() {
+    console.log("Welcome to JavaScript practice!");
 }
 
-myFunction(callbackFunction)
+greet("Mintu", sayWelcome);
+
+
+function square(num, callback) {
+    let result = num * num;
+    callback(result);
+}
+
+function displayResult(result) {
+    console.log("The result is: " + result);
+}
+
+square(6, displayResult);
