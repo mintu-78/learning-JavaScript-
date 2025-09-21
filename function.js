86ffbd5355471e7cbefe997d677d01e9ea02ffc7
Mintu <pmk80028@gmail.com>
2025-09-21 22:46:29 +0530

    practice functions in JavaScript

--- function.js
+++ function.js
@@ -1,108 +1,128 @@
-// function greet (){
-//     console.log("Hello JavaScript");
+// // function greet (){
+// //     console.log("Hello JavaScript");
     
-// }
-// greet()
+// // }
+// // greet()
 
 
 
-//***********function expression************//
+// //***********function expression************//
 
 
-// let greet = function (){
-//     console.log("hello javascript");
-// }
+// // let greet = function (){
+// //     console.log("hello javascript");
+// // }
 
-// greet();
+// // greet();
 
 
 
-//*********** Anonymous function expression************//
+// //*********** Anonymous function expression************//
  
 
-// const square = function(num){
-//     console.log(num * num);
+// // const square = function(num){
+// //     console.log(num * num);
     
-// }
-// square(8)
+// // }
+// // square(8)
 
 
 
 
-//*********** Arrow function************//
+// //*********** Arrow function************//
 
 
 
-// const greet = () => {
-//     console.log("hello");
+// // const greet = () => {
+// //     console.log("hello");
     
-// }
-// greet()
+// // }
+// // greet()
 
 
-// const square = (num) =>{
-//     console.log(num * num);
+// // const square = (num) =>{
+// //     console.log(num * num);
     
-// }
-// square(5)
+// // }
+// // square(5)
 
 
 
 
-// const add = (num1, num2) => console.log(num1 + num2);
+// // const add = (num1, num2) => console.log(num1 + num2);
 
-// add(5,7)
+// // add(5,7)
 
 
 
-// const add2 = (num1, num2) =>{
-//     console.log(num1 + num2);
+// // const add2 = (num1, num2) =>{
+// //     console.log(num1 + num2);
     
-// }
-// add2(5,5)
+// // }
+// // add2(5,5)
 
 
-// const sum = ( a, b) => {
-//     console.log(a + b);
+// // const sum = ( a, b) => {
+// //     console.log(a + b);
     
-// }
-// sum(100,100)
+// // }
+// // sum(100,100)
 
 
-// const data = function(num){
-//     console.log(num * num);
+// // const data = function(num){
+// //     console.log(num * num);
     
+// // }
+// // data(8, )
+
+
+// // let access = function (){
+// //     console.log("Hi Brothers");
+// // }
+
+// // access();
+
+// function subtract(a, b) {
+//   return a - b;
 // }
-// data(8, )
+
+// // console.log(subtract(10, 4)); // 6
 
 
-// let access = function (){
-//     console.log("Hi Brothers");
+
+// function divide(a, b) {
+//   return a / b;
 // }
 
-// access();
+// // console.log(divide(10, 2)); // 5
 
-function subtract(a, b) {
-  return a - b;
-}
+
+// function isPositive(num) {
+//   if (num > 0) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(isPositive(5));  // true
+// console.log(isPositive(-2)); // false
+
+
+// const max = (a, b) => (a > b ? 'a' : 'b');
+
+// console.log(max(15, 9));  // 15
+// console.log(max(4, 20));  // 20
+
+
+// const subtract = (a, b) => a - b;
 
 // console.log(subtract(10, 4)); // 6
+// console.log(subtract(5, 8));  // -3
 
 
+const checkSign = num => (num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero");
 
-function divide(a, b) {
-  return a / b;
-}
+console.log(checkSign(10));  // Positive
+console.log(checkSign(-5));  // Negative
+console.log(checkSign(0));   // Zero
 
-// console.log(divide(10, 2)); // 5
-
-
-function isPositive(num) {
-  if (num > 0) {
-    return true;
-  }
-  return false;
-}
-
-console.log(isPositive(5));  // true
-console.log(isPositive(-2)); // false
